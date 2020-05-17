469b34c4f733f6c5b821ce68e15ff7c18827f1e4
Santiago Vanegas Gil <savanegasg@gmail.com>
2020-05-17 13:46:02 -0500

    Add colors and some buttons

--- app/javascript/views/application/sections/home.vue
+++ app/javascript/views/application/sections/home.vue
@@ -1,59 +1,61 @@
 <template>
-  <section>
-    Logo
+<section>
+  <nav class='w-full container mx-auto flex justify-center'>
+    <img class='w-48 lg:w-64 xl:w-auto' src='img/logo.jpg'>
+  </nav>
 
-    <ul>
-      <li v-for='link in links'>
-        <a :key='link.id' :id='link.id' :href='link.link' target="_blank">{{ link.text }}</a>
-      </li>
-    </ul>
-  </section>
+  <!-- Buttons -->
+  <div v-for='product in products' class='m-2 leading-normal flex'>
+    <button class="btn btn-primary">
+      {{ product.text }}
+    </button>
+  </div>
+</section>
 </template>
 
 <script>
 export default {
   name: 'Home',
   data: () => ({
-    links: [
+    products: [{
+        id: 'caja-regalo',
+        text: 'Caja regalo día de las madres',
+        link: 'https://wa.link/tl3jh5'
+      },
       {
         id: 'cartilla-lettering',
-        text: "Cartilla de lettering",
-        link: "https://wa.link/8yd3qd"
+        text: 'Cartilla de lettering',
+        link: 'https://wa.link/8yd3qd'
       },
       {
         id: 'cartilla-dibujos',
-        text: "Cartilla de dibujos Timoteo",
-        link: "https://wa.link/xmuqln"
+        text: 'Cartilla de dibujos Timoteo',
+        link: 'https://wa.link/xmuqln'
       },
       {
         id: 'cartilla-timoteo',
-        text: "Cartilla de letra Timoteo",
-        link: "https://wa.link/gsljpo"
+        text: 'Cartilla de letra Timoteo',
+        link: 'https://wa.link/gsljpo'
       },
       {
         id: 'taller-virtual-timoteo',
-        text: "Taller virtual de letra Timoteo",
-        link: "https://wa.link/tt2dc1"
-      },
-      {
-        id: 'caja-regalo',
-        text: "Caja regalo día de las madres",
-        link: "https://wa.link/tl3jh5"
+        text: 'Taller virtual de letra Timoteo',
+        link: 'https://wa.link/tt2dc1'
       },
       {
         id: 'kits-marcadores',
-        text: "Kits de marcadores",
-        link: "https://wa.link/tfja2b"
+        text: 'Kits de marcadores',
+        link: 'https://wa.link/tfja2b'
       },
       {
         id: 'kits-timoteo',
-        text: "Kits de letra Timoteo",
-        link: "https://wa.link/p1yjc2"
+        text: 'Kits de letra Timoteo',
+        link: 'https://wa.link/p1yjc2'
       },
       {
         id: 'marcados-personalizados',
-        text: "Marcados personalizados",
-        link: "https://wa.link/ygy7q3"
+        text: 'Marcados personalizados',
+        link: 'https://wa.link/ygy7q3'
       },
     ]
   })
