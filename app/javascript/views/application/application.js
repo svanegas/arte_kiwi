933c3abc33f1cc4cbd5f693d438eba73c603bcd7
Felipe Tovar <f.tovar12@gmail.com>
2020-05-23 17:05:59 -0500

    Instantiate Vuetify but not use it yet

--- app/javascript/views/application/application.js
+++ app/javascript/views/application/application.js
@@ -5,9 +5,12 @@ import home from './sections/home.vue'
 
 import 'vuetify/dist/vuetify.min.css'
 
+// Vue.use(Vuetify)
+
 document.addEventListener('DOMContentLoaded', () => {
   const vm = new Vue({
     el: '#kiwi-vue',
+    vuetify: new Vuetify(),
     components: {
       'home': home
     }
