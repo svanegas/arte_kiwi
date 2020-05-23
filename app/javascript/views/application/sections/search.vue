2a562fd7699923094147c58657ff60d8b3ac8af8
Felipe Tovar <f.tovar12@gmail.com>
2020-05-23 12:50:15 -0500

    Set navbar for sp

--- app/javascript/views/application/sections/search.vue
+++ app/javascript/views/application/sections/search.vue
@@ -1,53 +1,11 @@
 <template>
-<section>
-  <nav class='w-full flex justify-center' style="max-height: 150px;">
-    <img class='object-contain w-48 md:w-56 lg:w-64 sm:my-8' src='img/logo.png'>
-  </nav>
-
-  <v-row align="center">
-    <v-col class="text-center" cols="12" sm="4">
-      <div class="my-2">
-        <v-btn small>Normal</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn small color="primary">Primary</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn small color="error">Error</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn small disabled>Disabled</v-btn>
-      </div>
-    </v-col>
-    <v-col class="text-center" cols="12" sm="4">
-      <div class="my-2">
-        <v-btn>Normal</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn color="primary">Primary</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn color="error">Error</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn disabled>Disabled</v-btn>
-      </div>
-    </v-col>
-    <v-col class="text-center" cols="12" sm="4">
-      <div class="my-2">
-        <v-btn large>Normal</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn large color="primary">Primary</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn large color="error">Error</v-btn>
-      </div>
-      <div class="my-2">
-        <v-btn large disabled>Disabled</v-btn>
-      </div>
-    </v-col>
-  </v-row>
+<section class="search">
+  <v-toolbar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-title><img class='logo' src='img/logo.png'></v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
 </section>
 </template>
 
@@ -58,3 +16,6 @@ export default {
   })
 }
 </script>
+<style lang="scss">
+  @import 'search';
+</style>
