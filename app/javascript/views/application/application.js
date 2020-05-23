dfc8582eee05941ad54fe11f1029df0b30bca5f5
Felipe Tovar <f.tovar12@gmail.com>
2020-05-23 16:58:13 -0500

    Add vuetify to application.js but not initialized

--- app/javascript/views/application/application.js
+++ app/javascript/views/application/application.js
@@ -1,5 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
+import Vuetify from 'vuetify'
+
 import home from './sections/home.vue'
+
+import 'vuetify/dist/vuetify.min.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   const vm = new Vue({
