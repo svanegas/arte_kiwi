487f7a446783b81a75a3eea279db2b8e924cb28a
Santiago Vanegas Gil <savanegasg@gmail.com>
2020-06-14 21:40:09 -0500

    Add social redirects

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,6 +1,15 @@
 class RedirectController < ApplicationController
 
-  def whatsapp
-    redirect_to 'https://wa.me/573175987665'
+  def show
+    case params[:social]
+    when :whatsapp
+      redirect_to 'https://wa.me/573175987665'
+    when :instagram
+      redirect_to 'https://www.instagram.com/arte_kiwi/'
+    when :facebook
+      redirect_to 'https://www.facebook.com/artekiwioficial'
+    else
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/whatsapp', to: 'redirect#whatsapp'
+  get '/whatsapp', to: 'redirect#show', social: :whatsapp
+  get '/instagram', to: 'redirect#show', social: :instagram
+  get '/facebook', to: 'redirect#show', social: :facebook
 end
