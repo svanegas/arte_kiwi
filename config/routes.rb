53ce9f1dd73ff522d3b9ebfb141b11f678eab3f8
Felipe Tovar <f.tovar12@gmail.com>
2020-06-16 12:11:56 -0500

    Commit to solve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/whatsapp', to: 'redirect#show', social: :whatsapp
+  get '/instagram', to: 'redirect#show', social: :instagram
+  get '/facebook', to: 'redirect#show', social: :facebook
   get '/newhome', to: "home#new_home"
 end
