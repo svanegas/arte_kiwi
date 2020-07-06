b47eaa9418693faeb8243f38002064a61129b7bd
Santiago Vanegas Gil <savanegasg@gmail.com>
2020-07-05 23:26:11 -0500

    Adds YouTube's channel short link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'buscar', to: 'search#index', as: :search
+  get '/buscar', to: 'search#index', as: :search
   get '/whatsapp', to: 'redirect#show', social: :whatsapp
   get '/instagram', to: 'redirect#show', social: :instagram
   get '/facebook', to: 'redirect#show', social: :facebook
+  get '/youtube', to: 'redirect#show', social: :youtube
 end
