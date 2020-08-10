51fcfe9cf39e49f1f5c49fb55c121717f8060dc3
Felipe Tovar <f.tovar12@gmail.com>
2020-08-10 08:36:19 -0500

    Working on seed script and install 2 gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p "-------------------------- Init seeds --------------------------"
+
+products_file = File.open("#{Rails.root}/app/javascript/views/search/products.json")
+products = JSON.load(products_file).dig("raw_products")
+products_file.close
+
+products.each do |pro|
+  category = Category.find_or_create_by(name: pro.dig("category"), label: pro.dig("label"))
+  product = Product.create(
+    name: pro.dig("name"),
+    gtm_id: pro.dig("id"),
+    quantity: pro.dig("quantity").to_i,
+    price: pro.dig("price"),
+    template: pro.dig("marker"),
+    data: {},
+    product_state: 1,
+    description: pro.dig("description").join("\n"),
+    category: category,
+  )
+end
+
+
+
+p "-------------------------- End seeds --------------------------"
