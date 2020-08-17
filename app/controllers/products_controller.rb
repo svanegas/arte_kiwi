468383eefd471aafa9d57786a54b8baa22620bf7
Felipe Tovar <f.tovar12@gmail.com>
2020-08-17 15:31:30 -0500

    Render 404 if product not found

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,16 @@ class ProductsController < ApplicationController
 
   def show
     product = Product.includes(:product_images).find_by(slug: params[:slug])
+    return render_404 if product.nil?
+
     @presenter = ProductPresenter::Show.new(product).process
+  end
+
+  private
+
+  def render_404
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
+    end
   end
 end
