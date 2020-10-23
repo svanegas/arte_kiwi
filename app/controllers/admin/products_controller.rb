454a4659816d48da51a0b1c58ed0e88f8ca5514f
Felipe Tovar <f.tovar12@gmail.com>
2020-10-23 15:24:43 -0500

    Change banners urls, change mapping of category and fix delete of products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -61,7 +61,7 @@ module Admin
     def destroy
       @product.destroy
       respond_to do |format|
-        format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+        format.html { redirect_to admin_products_url, notice: 'Product was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
--- app/javascript/packs/product_pack.js
+++ app/javascript/packs/product_pack.js
@@ -1,3 +1,5 @@
+require("@rails/ujs").start()
+
 import 'normalize.css';
 import 'lazysizes';
 import './../views/product/product.scss';
