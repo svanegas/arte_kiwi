7c5681133d65f51dfa88dc7782da96537fe5170d
Santiago Vanegas Gil <savanegasg@gmail.com>
2021-04-10 18:08:18 -0500

    Add CSV export for Shopify

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -10,6 +10,11 @@ module Admin
     # GET /products.json
     def index
       @products = Product.order(:name)
+
+      respond_to do |format|
+        format.html
+        format.csv { send_data @products.to_csv }
+      end
     end
 
     # GET /products/1
